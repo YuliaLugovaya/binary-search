9106023f57aea1d9844a095e07e74adf1d36ddfd
YuliaLugovaya <yulialugovayaphoto@gmail.com>
2023-04-11 18:43:44 +0300

    feat: add solutions

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 // Этот метод должен искать элемент в массиве и возвращать его индекс или -1, если элемент не находится в массиве.
 // Например, если ты ищешь G в массиве [A, B, C, D, E, F, G, H], твой метод должен возвращать 6. Если ты ищешь X, он должен возвращать -1.
 
-function binarySearch (){
+function binarySearch(arr, el, start, end) {
+  if (end < start) {
+    return -1;
+  }
 
+  let middle = Math.floor((start + end) / 2);
+
+  if (el === arr[middle]) {
+    return middle;
+  }
+
+  if (el < arr[middle]) {
+    return binarySearch(arr, el, start, middle - 1);
+  } else {
+    return binarySearch(arr, el, middle + 1, end);
+  }
 }
+console.log(binarySearch(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], 'G', 0, ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'].length));
 
 module.exports = binarySearch
--- spec/binarysearch.spec.js
+++ spec/binarysearch.spec.js
@@ -1,13 +1,25 @@
 const binarySearch = require('../index')
 
-test('Функция должна возвращать индекс элемента', () => {
-    
-})
-
-test('Функция должна быть определена', () => {
-    
-})
-
-test('Функция должна возвращать -1, если элемента не существует в данном массиве', () => {
-    
+describe("Function One:", () => {
+  test('Функция должна возвращать индекс элемента', () => {
+    let arr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+    let el = 'G';
+    let start = 0;
+    let end = arr.length;
+    expect(binarySearch(arr, el, start, end)).toBe(6);
+    expect(binarySearch(arr, el, start, end)).toEqual(6);
+    expect(binarySearch(arr, el, start, end)).toBeGreaterThan(3);
+  })
+  test('Функция должна быть определена', () => {
+    let arr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+    let el = 'G';
+    let start = 0;
+    let end = arr.length;
+    expect(binarySearch(arr, el, start, end)).toBeTruthy();
+    expect(binarySearch(arr, el, start, end)).toBeDefined();
+  })
+  test('Функция должна возвращать -1, если элемента не существует в данном массиве', () => {
+    expect(binarySearch([], 1, 0, [].length)).toBe(-1);
+    expect(binarySearch([1, 2, 7], 8, 0, [1, 2, 7].length)).toEqual(-1);
+  })
 })
